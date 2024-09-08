aab862409e0a9f5a1139da586c50e4c0fb9f209c
root <root@ip-172-31-23-107.us-west-2.compute.internal>
2024-09-08 04:08:13 +0000

    Adding Java & Python Project

---
+++ java/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Date currentDate = new Date();
+
+	System.out.println("Radhey Radhey! Mitron, Chalo JAnte hai aaj ki Date jo ki hai " + currentDate);
+
+	}
+
+}
